33275a44f3a6b1637b62a805ccb9ea426cfe3f34
376084782 <15988411694@163.com>
2016-03-05 20:37:30 +0800

    change index

---
+++ javascript/index.js
@@ -0,0 +1,57 @@
+window.onload=function(){
+	var tar=document.getElementsByClassName("now");
+	for(var i=0;i<tar.length;i++){
+		tar[i].addEventListener("click", paging);
+	}
+	}
+	function judge(t,j){
+			var Id=t.className.split(" ")[0].slice(4,5);
+			ID=parseInt(Id)+2*j;
+			console.log(ID);
+			if(document.getElementsByClassName("page"+ID)[0]!=null){
+				return 1;
+		}
+		else return 0;
+	}
+	function getPage(page){
+		return document.getElementsByClassName("page"+page)[0];
+	}
+	function paging(){
+		var t=this;
+		var i=0;
+		var j=-1;
+		var name=t.className.toString();
+		if(name.indexOf('right')!=-1){
+			j=1;
+		}
+		if(judge(t,j)==1){
+			var Id=t.className.split(" ")[0].slice(4,5);
+				ID=parseInt(Id);
+				getPage(ID+2*j).classList.add("next");
+				an=90*j;
+				getPage(ID+j).style.transform = 'rotateY('+an+'deg)';
+			var time=setInterval(function(){
+			if(i<90&&i>-90){
+				i+=j;
+			t.style.transform="rotateY("+i+"deg)";
+			var ang=-i/6;
+			t.style.transform+="skewY("+ang+"deg)";
+			}
+			else if(i<180&&i>=90||i<=-90&&i>-180){
+				console.log(i);
+					i+=j;
+					i2=j*i+180;
+					console.log(i2);
+				getPage(ID+j).style.transform = "rotateY("+i2+"deg)";
+				var ang=i2/6;
+				getPage(ID+j).style.transform+="skewY("+ang+"deg)";
+				}
+				else{
+					t.classList.remove("now");
+				getPage(ID+2*j).classList.add('now');
+				}
+		},10);
+			
+		}
+	}
+	
