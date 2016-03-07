2d0f890bdd62ed99922e83c4eb2b776ad4a5ef6a
376084782 <15988411694@163.com>
2016-03-07 16:54:56 +0800

    nearly done

--- javascript/index.js
+++ javascript/index.js
@@ -4,66 +4,91 @@ window.onload=function(){
 	function ini(){
 		var tar=document.getElementsByClassName("now");
 	for(var i=0;i<tar.length;i++){
-		tar[i].addEventListener("click", paging);
+		tar[i].addEventListener("click",flipP);
 	}
+	var tags=document.getElementsByClassName('tags')[0].getElementsByTagName('li');
+	for(var i=0;i<tags.length;i++){
+		tags[i].addEventListener("click", flipT);
 	}
-	function judge(t,j){
-			var Id=t.className.split(" ")[0].slice(4,5);
-			ID=parseInt(Id)+2*j;
-			if(document.getElementsByClassName("page"+ID)[0]!=null){
-				return 1;
-		}
-		else return 0;
 	}
 	function getPage(page){
 		return document.getElementsByClassName("page"+page)[0];
 	}
-	function paging(){
-		
+	function flipP(){
 		var t=this;
-		var i=0;
-		var j=-1;
-		var i2=-90*j;
-		var name=t.className.toString();
-		if(name.indexOf('right')!=-1){
-			j=1;
+		var Id=t.className.split(" ")[0].slice(4,5);
+		ID=parseInt(Id);
+		paging(ID);
+	} 
+	function flipT(){
+		var nows=document.getElementsByClassName('now');
+		var Id=nows[0].className.split(" ")[0].slice(4,5);
+		ID=parseInt(Id);
+		var tar=this.id.slice(4,5);
+		tarP=Math.floor((tar-ID)/2);
+		if(tarP<0){
+			ID=ID;
 		}
-		if(judge(t,j)==1){
-			var Id=t.className.split(" ")[0].slice(4,5);
-				ID=parseInt(Id);
-				an=90*j;
+		else{
+			ID+=1;
+		}
+		if(tarP!=0){
+			paging(ID,tarP);
+		}
+	}
+	function paging(ID,tarP){
+		var i=0;
+		var j=1;
+		
+		if(tarP==null){
+			tarP=1;
+		}
+		var name=getPage(ID).className.toString();
+		if(tarP<0||name.indexOf('left')!=-1){
+			j=-1;
+		}
+		tarP=Math.abs(tarP);
+		console.log(j);
+		var i2=Math.abs(90*j);
+		if(getPage(ID+(2*tarP-1)*j)){
+			an=90*j;
 			var time=null;	
 			var time=setInterval(function(){
 				
-				getPage(ID+2*j).classList.add('next');
+				getPage(ID+(2*tarP)*j).classList.add('next');
 			if(i<90&&i>-90){
 				i+=j;
-			t.style.transform="rotateY("+i+"deg)";
+			getPage(ID).style.transform="rotateY("+i+"deg)";
 			var ang=-i/6;
-			t.style.transform+="skewY("+ang+"deg)";
+			getPage(ID).style.transform+="skewY("+ang+"deg)";
 			}
 
 			else if(i<180&&i>=90||i<=-90&&i>-180){
 					i+=j;
 					i2-=1;
-					getPage(ID+j).classList.add('now');
-					getPage(ID+j).style.transform = 'rotateY('+an+'deg)';getPage(ID-j).classList.add('next');
-					getPage(ID-j).classList.remove("now");
-				getPage(ID+j).style.transform = "rotateY("+i2+"deg)";
+					getPage(ID+(2*tarP-1)*j).classList.add('now');
+					getPage(ID-j).classList.add("next");
+					getPage(ID+(2*tarP-1)*j).style.transform = 'rotateY('+an+'deg)';
+					
+				getPage(ID+(2*tarP-1)*j).style.transform = "rotateY("+i2+"deg)";
 				var ang=j*i2/6;
-				getPage(ID+j).style.transform+="skewY("+ang+"deg)";
+				getPage(ID+(2*tarP-1)*j).style.transform+="skewY("+ang+"deg)";
 				}
 				else{
 				clearInterval(time);
-				t.classList.remove("now");
-				getPage(ID+j).style.transform = "rotateY(0deg)";
-				getPage(ID-j).style.transform = 'rotateY(0deg)';
-				getPage(ID+2*j).classList.remove('next');
-				getPage(ID+2*j).classList.add('now');
-				getPage(ID-j).classList.remove('next');ini();
+				getPage(ID).classList.remove("now");
+				getPage(ID).style.transform="rotateY(0deg)";
+				getPage(ID).style.transform+="skewY(0deg)";
+				getPage(ID+(2*tarP-1)*j).style.transform = "rotateY(0deg)";
+				getPage(ID+(2*tarP-1)*j).style.transform = 'rotateY(0deg)';
+				getPage(ID+(2*tarP)*j).classList.add('now');
+				getPage(ID-j).classList.remove("now");
+				getPage(ID+(2*tarP)*j).classList.remove('next');
+				getPage(ID-j).classList.remove("next");
 				}
-		},10);
+		},1);
 			
 		}
+		ini();
 	}
 	
